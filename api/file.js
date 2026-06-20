3566afb8f677561704f3b70fe07843cdf670e6ea
X I Λ O C I Λ <xiaocia@outlook.co.id>
2026-06-20 16:29:57 +0700

    Enhance file response handling in API
    Update response headers to include content type and disposition for inline display.

--- api/file.js
+++ api/file.js
@@ -3,7 +3,6 @@ const axios = require('axios');
 const BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 
 module.exports = async (req, res) => {
-    // Jalankan CORS biar aman diakses dari mana saja
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
 
@@ -12,28 +11,28 @@ module.exports = async (req, res) => {
     }
 
     try {
-        // Mengambil nama file dari parameter URL (misal: nama-file.jpg)
         const { filename } = req.query;
 
         if (!filename) {
             return res.status(400).json({ status: false, message: 'Filename tidak disertakan.' });
         }
 
-        // Re-construct kembali ke path asli Telegram
-        // Karena di upload.js kita potong 'documents/', di sini kita kembalikan lagi
         const telegramFileUrl = `https://api.telegram.org/file/bot${BOT_TOKEN}/documents/${filename}`;
 
-        // Ambil file dari Telegram sebagai stream data
         const response = await axios({
             method: 'get',
             url: telegramFileUrl,
             responseType: 'stream'
         });
 
-        // Teruskan content-type asli dari Telegram ke browser user
-        res.setHeader('Content-Type', response.headers['content-type']);
+        // 1. Ambil content type asli (misal: image/png)
+        const contentType = response.headers['content-type'];
+        res.setHeader('Content-Type', contentType);
         
-        // Alirkan (pipe) data filenya langsung ke user
+        // 2. PAKSA HEADER BIAR INLINE (Buka di browser, bukan download otomatis)
+        res.setHeader('Content-Disposition', `inline; filename="${filename}"`);
+        
+        // Alirkan data filenya
         response.data.pipe(res);
 
     } catch (err) {
